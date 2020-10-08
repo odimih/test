47ab5885b5fb31ba52d65577b1a35afeb44e4860
Odysseas <odimih@gmail.com>
2020-10-08 11:09:30 +0300

    Test code

---
+++ hw.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int words = 0, wlen = 0;
+    char word[100];
+    string outstr;
+    cin >> words;
+    // FILE* fin = fopen("input.txt", "r"), * fout = fopen("output.txt", "w+");
+    // fscanf(fin, "%d", &words);
+    // TODO: add comment here
+    // FIXME: change sth in code
+    for (int i = 0; i < words; i++)
+    {
+        cin >> word;
+        // fscanf(fin, "%s", &word);
+        wlen = strlen(word);
+        if (wlen > 10)
+        {
+            cout << word << endl;
+            cout << wlen << endl;
+            outstr = word[0] + to_string(wlen - 2) + word[wlen - 1];
+            strcpy(word, outstr.c_str());
+            cout << word << endl;
+            // fprintf(fout, "%s%s", word, "\n");
+        }
+        else
+        {
+            cout << word << endl;
+            // fprintf(fout, "%s%s", word, "\n");
+        }
+    }
+    cin.ignore();
+}
